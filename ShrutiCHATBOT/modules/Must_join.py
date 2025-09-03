8350c7901d48b73774702aaf888abd78b25d2f52
AkashOP <akp954834@gmail.com>
2025-09-03 20:42:57 +0530

    Update Must_join.py

--- ShrutiCHATBOT/modules/Must_join.py
+++ ShrutiCHATBOT/modules/Must_join.py
@@ -1,14 +1,19 @@
-
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 from ShrutiCHATBOT import ShrutiCHATBOT as app
-from config import UPDATE_CHNL as MUST_JOIN
+from config import UPDATE_CHNL as MUST_JOIN, OWNER_ID
+
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
+    # 🚨 Bypass must_join for OWNER
+    if msg.from_user and msg.from_user.id == int(OWNER_ID):
+        return
+
     if not MUST_JOIN:
         return
+
     try:
         try:
             await app.get_chat_member(MUST_JOIN, msg.from_user.id)
@@ -18,12 +23,18 @@ async def must_join_channel(app: Client, msg: Message):
             else:
                 chat_info = await app.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
+
             try:
                 await msg.reply_photo(
                     photo="https://envs.sh/EuZ.jpg",
-                    caption=(f"**👋 ʜᴇʟʟᴏ {msg.from_user.mention},**\n\n**ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ᴛʜᴇ [ᴄʜᴀɴɴᴇʟ]({link}) ᴛᴏ sᴇɴᴅ ᴍᴇssᴀɢᴇs ɪɴ ᴛʜɪs ʙᴏᴛ.**"),
-                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("๏ ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ ๏", url=link)]]))
-        
+                    caption=(
+                        f"**👋 ʜᴇʟʟᴏ {msg.from_user.mention},**\n\n"
+                        f"**ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ᴛʜᴇ [ᴄʜᴀɴɴᴇʟ]({link}) ᴛᴏ sᴇɴᴅ ᴍᴇssᴀɢᴇs ɪɴ ᴛʜɪs ʙᴏᴛ.**"
+                    ),
+                    reply_markup=InlineKeyboardMarkup(
+                        [[InlineKeyboardButton("๏ ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ ๏", url=link)]]
+                    )
+                )
                 await msg.stop_propagation()
             except ChatWriteForbidden:
                 pass
